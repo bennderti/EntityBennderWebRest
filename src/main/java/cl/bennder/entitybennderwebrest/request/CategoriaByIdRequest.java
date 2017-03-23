7e2f625f04b2039c189611272382d4d89c706fee
bennderti <bennder.ti@gmail.com>
2017-03-23 01:22:47 -0300

    - Request de obtención de categoria por id

---
+++ src/main/java/cl/bennder/entitybennderwebrest/request/CategoriaByIdRequest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.bennder.entitybennderwebrest.request;
+
+/**
+ *
+ * @author dyanez
+ */
+public class CategoriaByIdRequest{
+    private Integer idCategoria;
+
+    public CategoriaByIdRequest() {
+    }
+
+    public CategoriaByIdRequest(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+    
+}
