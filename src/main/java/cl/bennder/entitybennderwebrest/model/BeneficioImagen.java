a31b321dfa38b5c5f96115103dbcaebd022f691e
bennderti <bennder.ti@gmail.com>
2017-03-26 12:20:26 -0300

    - Clases utulizadas para cargar imagenes a los beneficios, marcando como principal la orden 1

---
+++ src/main/java/cl/bennder/entitybennderwebrest/model/BeneficioImagen.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.bennder.entitybennderwebrest.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author dyanez
+ */
+public class BeneficioImagen implements Serializable{
+    private Integer idBeneficio;
+    private String nombre;
+    private byte[] imagen;
+    private Integer orden;//1 es considerado como imagen principal
+
+    public BeneficioImagen() {
+    }
+
+    public BeneficioImagen(Integer idBeneficio, String nombre, byte[] imagen, Integer orden) {
+        this.idBeneficio = idBeneficio;
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.orden = orden;
+    }
+
+    public Integer getIdBeneficio() {
+        return idBeneficio;
+    }
+
+    public void setIdBeneficio(Integer idBeneficio) {
+        this.idBeneficio = idBeneficio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public byte[] getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(byte[] imagen) {
+        this.imagen = imagen;
+    }
+
+    public Integer getOrden() {
+        return orden;
+    }
+
+    public void setOrden(Integer orden) {
+        this.orden = orden;
+    }
+    
+    
+}
---
+++ src/main/java/cl/bennder/entitybennderwebrest/request/UploadBeneficioImagenRequest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.bennder.entitybennderwebrest.request;
+
+import cl.bennder.entitybennderwebrest.model.BeneficioImagen;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author dyanez
+ */
+public class UploadBeneficioImagenRequest implements Serializable{
+    private List<BeneficioImagen> beneficioImagenes;
+
+    public UploadBeneficioImagenRequest() {
+    }
+
+    public UploadBeneficioImagenRequest(List<BeneficioImagen> beneficioImagenes) {
+        this.beneficioImagenes = beneficioImagenes;
+    }
+
+    public List<BeneficioImagen> getBeneficioImagenes() {
+        return beneficioImagenes;
+    }
+
+    public void setBeneficioImagenes(List<BeneficioImagen> beneficioImagenes) {
+        if(this.beneficioImagenes == null){
+            this.beneficioImagenes = new ArrayList<>();
+        }
+        this.beneficioImagenes = beneficioImagenes;
+    }
+    
+}
