2a9507454ac2f53974c4c379c6b5245384d8eba9
Diego <diego.riveros.pinilla@gmail.com>
2017-03-30 01:24:03 -0300

    se agregan contructores

--- src/main/java/cl/bennder/entitybennderwebrest/request/BeneficioRequest.java
+++ src/main/java/cl/bennder/entitybennderwebrest/request/BeneficioRequest.java
@@ -7,6 +7,13 @@ public class BeneficioRequest extends UserRequest {
 
     private Integer idBeneficio;
 
+    public BeneficioRequest() {
+    }
+
+    public BeneficioRequest(Integer idBeneficio) {
+        this.idBeneficio = idBeneficio;
+    }
+
     public Integer getIdBeneficio() {
         return idBeneficio;
     }
