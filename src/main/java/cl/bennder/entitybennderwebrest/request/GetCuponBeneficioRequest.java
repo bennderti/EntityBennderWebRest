f2957708501743aeae8d72534cf7f9f2611e988d
bennderti <bennder.ti@gmail.com>
2017-03-30 23:47:45 -0300

    - Clases para generar QR/Obtención cuón- Avance I

---
+++ src/main/java/cl/bennder/entitybennderwebrest/request/GetCuponBeneficioRequest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.bennder.entitybennderwebrest.request;
+
+/**
+ *
+ * @author dyanez
+ */
+public class GetCuponBeneficioRequest extends UserRequest{
+    private Integer idBeneficio;
+    private Integer cantidad;
+
+    public GetCuponBeneficioRequest() {
+        this.cantidad = 1;
+    }
+
+    public GetCuponBeneficioRequest(Integer idBeneficio) {
+        this.idBeneficio = idBeneficio;
+    }
+
+    public GetCuponBeneficioRequest(Integer idBeneficio, Integer idUsuario) {
+        super(idUsuario);
+        this.idBeneficio = idBeneficio;
+    }
+
+    public Integer getIdBeneficio() {
+        return idBeneficio;
+    }
+
+    public void setIdBeneficio(Integer idBeneficio) {
+        this.idBeneficio = idBeneficio;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "GetCuponBeneficioRequest{" + "idBeneficio=" + idBeneficio + ", cantidad=" + cantidad + '}';
+    }
+    
+    
+    
+    
+}
---
+++ src/main/java/cl/bennder/entitybennderwebrest/response/GeneraQrResponse.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.bennder.entitybennderwebrest.response;
+
+/**
+ *
+ * @author dyanez
+ */
+public class GeneraQrResponse extends ValidacionResponse{
+
+    public GeneraQrResponse() {
+    }
+    
+}
