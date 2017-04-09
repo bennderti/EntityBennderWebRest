9c1f8712eb8d47f89dbd9f8e05bca0e67f646aa1
Marcos <Marcos@MSI-MARCOS>
2017-04-09 13:12:01 -0300

    MG - clase de request para guardar preferencias

---
+++ src/main/java/cl/bennder/entitybennderwebrest/request/GuardarPreferenciasRequest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.bennder.entitybennderwebrest.request;
+
+import java.io.Serializable;
+import cl.bennder.entitybennderwebrest.model.Categoria;
+import java.util.List;
+
+/**
+ *
+ * @author Marcos
+ */
+public class GuardarPreferenciasRequest implements Serializable {
+    
+    private String emailAdicional;
+    private Integer fonoAdicional;
+    private Integer IdEstadoCivil;
+    private List<Categoria> categoriasSeleccionadas;
+
+    public String getEmailAdicional() {
+        return emailAdicional;
+    }
+
+    public void setEmailAdicional(String emailAdicional) {
+        this.emailAdicional = emailAdicional;
+    }
+
+    public Integer getFonoAdicional() {
+        return fonoAdicional;
+    }
+
+    public void setFonoAdicional(Integer fonoAdicional) {
+        this.fonoAdicional = fonoAdicional;
+    }
+
+    public Integer getIdEstadoCivil() {
+        return IdEstadoCivil;
+    }
+
+    public void setIdEstadoCivil(Integer IdEstadoCivil) {
+        this.IdEstadoCivil = IdEstadoCivil;
+    }
+
+    public List<Categoria> getCategoriasSeleccionadas() {
+        return categoriasSeleccionadas;
+    }
+
+    public void setCategoriasSeleccionadas(List<Categoria> categoriasSeleccionadas) {
+        this.categoriasSeleccionadas = categoriasSeleccionadas;
+    }
+}
